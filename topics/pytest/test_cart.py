0ece76e38181f04ea4fe89782e4aaec897c32322
clemensharasewycz <clemens.harasewycz@polytech.com>
2023-07-19 16:20:02 +0200

    added tutorial scripts of pytest video tutrial

---
+++ topics/pytest/test_cart.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def testAddItemtoCart(tc_setup):           # we pass the fixture into the test -> this will be executed prior when running the test 
+    print("Add Item Successful")
+
+def testRemoveItemFromCart(tc_setup): 
+    print("Remove Item Successfull")
---
+++ topics/pytest/test_login.py
@@ -0,0 +1,18 @@
+import pytest
+
+@pytest.mark.skip
+def testLogin():
+    print("Login Successful")
+
+def testLogoff():
+    print("Logoff Successful")
+
+@pytest.mark.sanity
+def testCalculation():
+    assert 2+2 == 4
+
+@pytest.mark.xfail          # mark this test as an expected failure (e.g. if function to be tested has not been defined yet)
+def testCalculation1():
+    assert 2+2 == 5
+
+print("This script ran")
