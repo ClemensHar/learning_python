8da63577dc09251716d8a78115ffec0aaf4c8c21
Clemens Harasewycz <57918776+ClemensHar@users.noreply.github.com>
2023-08-08 09:20:07 +0200

    Pytest tutorial (#2)
    * added tutorial scripts of pytest video tutrial
    
    * example for applying pytest

---
+++ topics/pytest/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+@pytest.fixture(scope="session", autouse=True)
+def tc_setup(): 
+    print("Launch browser")
+    print("Login")
+    print("Browse products")
+    yield  # statements beyond the yield, will be performed after the test -> teardown 
+    print("Logoff")
+    print("Close browser")
